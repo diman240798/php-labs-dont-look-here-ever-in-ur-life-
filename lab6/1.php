aa1c6ea7bdfd3b1440ec7735628c0f2b523ee112
Dmitriy <diman240798@yandex.ru>
2020-03-15 15:34:23 +0300

    lab 6 finished

--- lab6/1.php
+++ lab6/1.php
@@ -2,11 +2,47 @@
 
 mt_srand((double)microtime() * 1000000);
 
-$min = 10;
-$max = 100;
 
-$width = 20;
-$maxHeight = 400;
+$width = 40;
+$minHeight = 10;
+$maxHeight = 250;
+
+$font = 'C:\Users\diman\PhpstormProjects\lab9\lab6\arial.ttf';
+
+$image = imagecreatetruecolor($width, $maxHeight);
+$white = imagecolorallocate($image, 255, 255, 255);
+$black = imagecolorallocate($image, 0, 0, 0);
+
+imagefilledrectangle($image, 0, 0, $width, $maxHeight, $white);
+imagettftext($image, 20, 0, 10,  30, $black, $font, "Y");
+imagepng($image, "./x.png");
+
+for ($i = 0; $i < 5; $i++) {
+    $randHeight = mt_rand($minHeight, $maxHeight);
+    $image = imagecreatetruecolor($width, $maxHeight);
+
+    $colors = createColors($image);
+
+    $white = imagecolorallocate($image, 255, 255, 255);
+
+    imagefilledrectangle($image, 0, 0, $width, $maxHeight, $white);
+    $y1 = $maxHeight - $randHeight;
+    error_log($y1);
+    error_log($randHeight);
+    error_log($maxHeight);
+    imagefilledrectangle($image, 0, $y1, $width, $maxHeight, $colors[$i]);
+
+    imagepng($image, "./image$i.png");
+}
+
+$image = imagecreatetruecolor($width, $maxHeight);
+$white = imagecolorallocate($image, 255, 255, 255);
+$black = imagecolorallocate($image, 0, 0, 0);
+
+imagefilledrectangle($image, 0, 0, $width, $maxHeight, $white);
+imagettftext($image, 20, 0, 10,  $maxHeight - 5, $black, $font, "X");
+imagepng($image, "./y.png");
+
 
 function createColors($im) {
 
@@ -20,34 +56,17 @@ function createColors($im) {
 
     return $colors;
 }
-
-for ($i = 0; $i < 5; $i++) {
-    $randHeight = mt_rand($min, $max);
-    $height = $randHeight / 100 * $maxHeight;
-    $image = imagecreatetruecolor($width, $maxHeight);
-
-    $colors = createColors($image);
-
-    $white = imagecolorallocate($image, 255, 255, 255);
-
-    imagefilledrectangle($image, 0, 0, $width, $maxHeight, $white);
-    $y1 = $maxHeight - $height;
-    error_log($y1);
-    error_log($height);
-    error_log($maxHeight);
-    imagefilledrectangle($image, 0, $y1, $width, $maxHeight, $colors[$i]);
-
-    imagepng($image, "./image$i.png");
-}
 ?>
 <html>
     <body>
-        <div>
+        <div style="border: 4px  double black;width: 310px">
+            <img src="/lab6/x.png" />
             <img src="/lab6/image0.png" />
             <img src="/lab6/image1.png" />
             <img src="/lab6/image2.png" />
             <img src="/lab6/image3.png" />
             <img src="/lab6/image4.png" />
+            <img src="/lab6/y.png" />
         </div>
     </body>
 </html>
