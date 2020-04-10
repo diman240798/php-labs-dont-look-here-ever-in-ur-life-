daf1f46787dd0e0e198fda74a8cf38a003fedf3f
diman240798 <diman240798>
2020-04-10 10:36:42 +0300

    lab 8

--- lab7/form.php
+++ lab7/form.php
@@ -1,61 +1,7 @@
 <?php
 require("form_func.php")
 ?>
-
-<style>
-    form {
-        background-color: #FFF;
-        height: 400px;
-        width: 380px;
-        margin-right: auto;
-        margin-top: 0px;
-        border-top-left-radius: 10px;
-        border-top-right-radius: 10px;
-        padding: 0px;
-        text-align:center;
-    }
-    label {
-        font-family: Georgia, "Times New Roman", Times, serif;
-        font-size: 16px;
-        color: #333;
-        height: 20px;
-        width: 150px;
-        margin-top: 10px;
-        text-align: left;
-        clear: both;
-        float:left;
-        margin-right:15px;
-    }
-    .inpStr {
-        height: 20px;
-        width: 150px;
-        border: 1px solid #000;
-        margin-top: 10px;
-        float: left;
-    }
-
-    .inpCheck {
-        height: 20px;
-        width: 50px;
-        margin-top: 5px;
-        float: left;
-    }
-    input[type=submit] {
-        margin: 20px;
-        float:right;
-    }
-    select {
-        height: 80px;
-        width: 200px;
-        border: 1px solid #000;
-        margin-top: 10px;
-        float: left;
-        overflow-y: auto;
-    }
-    .longLabel {
-        width: 400px;
-    }
-</style>
+<link href="styles.css" rel="stylesheet"/>
 
 <html>
     <body>
